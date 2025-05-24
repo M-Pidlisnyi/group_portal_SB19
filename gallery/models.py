2e44ebcfebcb6a8f54bff25f6ff308c4c34184ef
vladyslavvvvvvv <zebrochka228333@gmail.com>
2025-05-24 18:53:56 +0200

    updated models and added to views.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Student(models.BaseModel):
+class Student(models.Model):
     name = models.CharField(max_length=100)
-class Photo(models.BaseModel):
+class Photo(models.Model):
     photo = models.ImageField(upload_to="", blank=True, null=True)
-class Video(models.BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField()
+    class Meta:
+        verbose_name = "photos"
+class Video(models.Model):
     video = models.FileField(upload_to="", blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE )
+    uploaded_at = models.DateTimeField()
+    class Meta:
+        verbose_name = "videos"
--- gallery/views.py
+++ gallery/views.py
@@ -3,4 +3,19 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
 
-class PhotoCreateView()
+from .models import Photo, Video
+
+def double_list(request:HttpRequest):
+    first_list = Photo.objects.filter(photo="")
+    second_list = Video.objects.all()
+
+    return render(request, "gallery, media_list.html")
+
+class PhotoCreateView(CreateView):
+    model = Photo
+    template_name = "gallery/photo_create.html"
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.user = user
+
+    
